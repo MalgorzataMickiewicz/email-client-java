39816ebd8cf149961c6f6cc18bfd7ff89b57fd4a
Malgorzata Mickiewicz <mickiewicz.programista@gmail.com>
2021-02-01 15:18:38 +0100

    Working on reading messages

--- src/com/mm/Launcher.java
+++ src/com/mm/Launcher.java
@@ -2,9 +2,6 @@ package com.mm;
 
 import com.mm.view.ViewFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
